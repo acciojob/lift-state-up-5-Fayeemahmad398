7532adca9a529ff9dbf28f8cb793f2eb02486276
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-07-16 16:03:03 +0530

    submitting code-Sun Jul 16 2023 16:03:00 GMT+0530 (India Standard Time)

---
+++ src/components/Parent.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import React from "react";
+import Child from "./Child";
+const Parent = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [inputContent, setContent] = useState({});
+
+  const handleLogin = () => {
+    if (inputContent.username && inputContent.password) {
+      setLoggedIn(true);
+    }
+  };
+
+  return (
+    <div>
+      {!isLoggedIn ? (
+        <div>
+          <label htmlFor="">Username:</label>
+          <input
+            type="text"
+            onChange={(event) => {
+              setContent({ ...inputContent, username: event.target.value });
+            }}
+          />
+          <label htmlFor="">Password:</label>
+          <input
+            type="password"
+            onChange={(event) => {
+              setContent({
+                ...inputContent,
+                password: event.target.value,
+              });
+            }}
+          />
+
+          <button onClick={handleLogin}> login</button>
+        </div>
+      ) : (
+        <Child islogin={isLoggedIn} />
+      )}
+    </div>
+  );
+};
+
+export default Parent;
